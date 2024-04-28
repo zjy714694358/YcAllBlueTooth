264cf633552a16f96677922a07b2ed8565bb6943
Administrator <714694358@qq.com>
2024-04-28 16:02:03 +0800

    变比单相

---
+++ app/src/main/java/com/yc/allbluetooth/bianbi/activity/danxiang/DxBbActivity.java
@@ -0,0 +1,16 @@
+package com.yc.allbluetooth.bianbi.activity.danxiang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yc.allbluetooth.R;
+
+public class DxBbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dx_bb);
+    }
+}
